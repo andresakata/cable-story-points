db4111fdc80cad4153f90922af42eddeba836741
André Guimarães Sakata <andregsakata@gmail.com>
2020-01-21 23:39:19 -0300

    Load connected users

--- app/controllers/planning_room_controller.rb
+++ app/controllers/planning_room_controller.rb
@@ -4,6 +4,9 @@ class PlanningRoomController < ApplicationController
   before_action :require_login
 
   def index
+    @users = ActionCable.server.connections.collect(&:current_user).uniq
+  end
+
   end
 
   private
